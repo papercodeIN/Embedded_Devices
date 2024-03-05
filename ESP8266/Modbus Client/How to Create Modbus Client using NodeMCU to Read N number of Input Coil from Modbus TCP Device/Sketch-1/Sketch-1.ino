d56b6ebec5a03ffbda5cf0c68d9223365f542c87
Fusion Automate <eng.innovativ@gmail.com>
2024-03-05 19:06:28 +0530

    Create Sketch-1.ino

---
+++ ESP8266/Modbus Client/How to Create Modbus Client using NodeMCU to Read N number of Input Coil from Modbus TCP Device/Sketch-1/Sketch-1.ino
@@ -0,0 +1,64 @@
+#include <ESP8266WiFi.h>
+#include <ModbusIP_ESP8266.h>
+
+const char* ssid = "Capgemini_4G";
+const char* password = "MN704116";
+
+IPAddress remote(192, 168, 1, 7);  // Address of Modbus Slave device
+const int START_INPUT = 0;           // Starting input address
+const int NUM_INPUTS = 10;           // Number of inputs to read
+const int INTERVAL = 1000;         // Interval between reads (in milliseconds)
+
+WiFiClient client;
+ModbusIP mb;
+
+bool res[NUM_INPUTS];
+uint32_t LastModbusRequest = 0;  // Variable to track the last Modbus request time
+
+void setup() {
+  Serial.begin(115200);
+
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Initialize Modbus client
+  mb.client();
+}
+
+void loop() {
+  if (millis() - LastModbusRequest >= INTERVAL) {
+    LastModbusRequest = millis();
+
+    if (mb.isConnected(remote)) {  
+      // Read input status from Modbus Slave
+      uint8_t transaction = mb.readIsts(remote, START_INPUT, res, NUM_INPUTS, nullptr, 1);
+      
+      // Wait for the transaction to complete
+      while(mb.isTransaction(transaction)) {
+        mb.task();
+        delay(10);
+      }
+
+      // Print input status values
+      Serial.println("Input Status Values:");
+      for (int i = 0; i < NUM_INPUTS; i++) {
+        Serial.print("Input ");
+        Serial.print(i);
+        Serial.print(": ");
+        Serial.println(res[i]);
+      }
+    } else {
+      // If not connected, try to connect
+      mb.connect(remote);
+    }
+  }
+}
