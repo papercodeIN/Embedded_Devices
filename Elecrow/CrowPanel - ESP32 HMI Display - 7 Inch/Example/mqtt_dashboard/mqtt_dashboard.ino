c5097dbe2aface8e8df54ca64d94dacf9aaaefb5
Fusion Automate <eng.innovativ@gmail.com>
2025-02-03 16:41:09 +0530

    Added MQTT Dashboard Development Code - WIP

---
+++ Elecrow/CrowPanel - ESP32 HMI Display - 7 Inch/Example/mqtt_dashboard/mqtt_dashboard.ino
@@ -0,0 +1,228 @@
+#include <PCA9557.h>
+#include <lvgl.h>
+#include <LovyanGFX.hpp>
+#include <lgfx/v1/platforms/esp32s3/Panel_RGB.hpp>
+#include <lgfx/v1/platforms/esp32s3/Bus_RGB.hpp>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include "ui.h"
+#include <WiFi.h>
+
+const char* ssid = "Capgemini_4G";  
+const char* password = "MN704116"; // Set a password  
+
+// These settings will affect the IP address allocation for devices connected to the Access Point  
+IPAddress local_ip(192, 168, 1, 67);  // The ESP32's IP address as an Access Point is typically 192.168.4.1  
+IPAddress gateway(192, 168, 1, 1);    // Gateway address  
+IPAddress subnet(255, 255, 255, 0);   // Subnet mask 
+
+class LGFX : public lgfx::LGFX_Device
+{
+public:
+  lgfx::Bus_RGB     _bus_instance;
+  lgfx::Panel_RGB   _panel_instance;
+
+  LGFX(void)
+  {
+    {
+      auto cfg = _bus_instance.config();
+      cfg.panel = &_panel_instance;
+      
+      cfg.pin_d0  = GPIO_NUM_15; // B0
+      cfg.pin_d1  = GPIO_NUM_7;  // B1
+      cfg.pin_d2  = GPIO_NUM_6;  // B2
+      cfg.pin_d3  = GPIO_NUM_5;  // B3
+      cfg.pin_d4  = GPIO_NUM_4;  // B4
+      
+      cfg.pin_d5  = GPIO_NUM_9;  // G0
+      cfg.pin_d6  = GPIO_NUM_46; // G1
+      cfg.pin_d7  = GPIO_NUM_3;  // G2
+      cfg.pin_d8  = GPIO_NUM_8;  // G3
+      cfg.pin_d9  = GPIO_NUM_16; // G4
+      cfg.pin_d10 = GPIO_NUM_1;  // G5
+      
+      cfg.pin_d11 = GPIO_NUM_14; // R0
+      cfg.pin_d12 = GPIO_NUM_21; // R1
+      cfg.pin_d13 = GPIO_NUM_47; // R2
+      cfg.pin_d14 = GPIO_NUM_48; // R3
+      cfg.pin_d15 = GPIO_NUM_45; // R4
+
+      cfg.pin_henable = GPIO_NUM_41;
+      cfg.pin_vsync   = GPIO_NUM_40;
+      cfg.pin_hsync   = GPIO_NUM_39;
+      cfg.pin_pclk    = GPIO_NUM_0;
+      cfg.freq_write  = 15000000;
+
+      cfg.hsync_polarity    = 0;
+      cfg.hsync_front_porch = 40;
+      cfg.hsync_pulse_width = 48;
+      cfg.hsync_back_porch  = 40;
+      
+      cfg.vsync_polarity    = 0;
+      cfg.vsync_front_porch = 1;
+      cfg.vsync_pulse_width = 31;
+      cfg.vsync_back_porch  = 13;
+
+      cfg.pclk_active_neg   = 1;
+      cfg.de_idle_high      = 0;
+      cfg.pclk_idle_high    = 0;
+
+      _bus_instance.config(cfg);
+    }
+    {
+      auto cfg = _panel_instance.config();
+      cfg.memory_width  = 800;
+      cfg.memory_height = 480;
+      cfg.panel_width  = 800;
+      cfg.panel_height = 480;
+      cfg.offset_x = 0;
+      cfg.offset_y = 0;
+      _panel_instance.config(cfg);
+    }
+    _panel_instance.setBus(&_bus_instance);
+    setPanel(&_panel_instance);
+  }
+};
+
+LGFX lcd;
+//UI
+#define TFT_BL 2
+SPIClass& spi = SPI;
+#include "touch.h"
+
+/* Change to your screen resolution */
+static uint32_t screenWidth;
+static uint32_t screenHeight;
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t disp_draw_buf[800 * 480 / 15];
+static lv_disp_drv_t disp_drv;
+
+/* Display flushing */
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+{
+  uint32_t w = (area->x2 - area->x1 + 1);
+  uint32_t h = (area->y2 - area->y1 + 1);
+
+#if (LV_COLOR_16_SWAP != 0)
+ lcd.pushImageDMA(area->x1, area->y1, w, h,(lgfx::rgb565_t*)&color_p->full);
+#else
+  lcd.pushImageDMA(area->x1, area->y1, w, h,(lgfx::rgb565_t*)&color_p->full);
+#endif
+
+  lv_disp_flush_ready(disp);
+}
+
+void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
+{
+  if (touch_has_signal())
+  {
+    if (touch_touched())
+    {
+      data->state = LV_INDEV_STATE_PR;
+      data->point.x = touch_last_x;
+      data->point.y = touch_last_y;
+      Serial.print("Data x ");
+      Serial.println(data->point.x);
+      Serial.print("Data y ");
+      Serial.println(data->point.y);
+    }
+    else if (touch_released())
+    {
+      data->state = LV_INDEV_STATE_REL;
+    }
+  }
+  else
+  {
+    data->state = LV_INDEV_STATE_REL;
+  }
+  delay(15);
+}
+
+void setup()
+{
+  Serial.begin(9600);
+
+  pinMode(38, OUTPUT);
+
+  Wire.begin(19, 20);
+  lcd.begin();
+  lcd.fillScreen(TFT_BLACK);
+  lcd.setTextSize(2);
+  delay(200);
+  lv_init();
+
+  touch_init();
+
+  screenWidth = lcd.width();
+  screenHeight = lcd.height();
+
+  lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, screenWidth * screenHeight / 15);
+  lv_disp_drv_init(&disp_drv);
+  disp_drv.hor_res = screenWidth;
+  disp_drv.ver_res = screenHeight;
+  disp_drv.flush_cb = my_disp_flush;
+  disp_drv.draw_buf = &draw_buf;
+  lv_disp_drv_register(&disp_drv);
+
+  static lv_indev_drv_t indev_drv;
+  lv_indev_drv_init(&indev_drv);
+  indev_drv.type = LV_INDEV_TYPE_POINTER;
+  indev_drv.read_cb = my_touchpad_read;
+  lv_indev_drv_register(&indev_drv);
+
+#ifdef TFT_BL
+  ledcSetup(1, 300, 8);
+  ledcAttachPin(TFT_BL, 1);
+  ledcWrite(1, 255);
+#endif
+ 
+#ifdef TFT_BL
+  pinMode(TFT_BL, OUTPUT);
+  digitalWrite(TFT_BL, LOW); 
+  delay(500);
+  digitalWrite(TFT_BL, HIGH);
+#endif
+
+  ui_init();
+
+  // Set to Access Point mode  
+  WiFi.mode(WIFI_AP);  
+  
+  // Configure Access Point parameters  
+  WiFi.softAPConfig(local_ip, gateway, subnet);  
+  WiFi.softAP(ssid, password);  
+  
+  // Print Access Point information  
+  Serial.print("AP IP address: ");  
+  Serial.println(WiFi.softAPIP());  
+  // Get the IP address of the softAP
+  String ipAddress = WiFi.softAPIP().toString();
+
+  // Set the label text with the IP address
+  lv_label_set_text(ui_ipaddress, ipAddress.c_str());  
+
+  // Create a timer to update the arc value every 5 seconds (5000 ms)
+  lv_timer_t *timer = lv_timer_create(update_arc_value, 5000, NULL);
+
+  lv_timer_handler();
+}
+
+// Timer callback function to update the arc value
+void update_arc_value(lv_timer_t *timer) {
+  int newValue = random(0, 100);  // Generate a random value between 0 and 100
+  lv_arc_set_value(ui_Arc1, newValue);  // Update the arc value
+  
+  // Convert the integer value to a string
+  char valueStr[4];  // Enough space for 3 digits and null terminator
+  snprintf(valueStr, sizeof(valueStr), "%d", newValue);
+
+  // Update the LVGL label text
+  lv_label_set_text(ui_Label1, valueStr);
+
+}
+
+void loop()
+{
+  lv_timer_handler();
+  delay(10);
+}
